735feb6dadf0a1187e5eb268851de2beeb18f571
Williane Delfino <willy.pinheiro.94@gmail.com>
2023-04-14 11:50:31 -0300

    atualizei html, inseri scripts.js

---
+++ scripts.js
@@ -0,0 +1,9 @@
+const button = document.getElementsByTagName('convert-button')
+const dolar = 5.2
+
+const convertValues = () => {
+    const input = document.getElementById('input-real').value
+    
+}
+
+button.addEventListener('click', convertValues)
